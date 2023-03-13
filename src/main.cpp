810ed836661ada7949c937b70b1f87bf141b02a1
Michael Kan <michael.kan.mail@gmail.com>
2023-03-13 11:49:59 +0100

    addition

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "uuint_t.h"
 
 int main() {
-	ubc::uuint_t n{9223372036854775808};
+	ubc::uuint_t n{100};
+	ubc::uuint_t n2{980};
 	std::cout << "hi" << std::endl;
+	n.Add(n2);
+	
 }
--- src/uuint_t.cpp
+++ src/uuint_t.cpp
@@ -33,7 +33,37 @@ namespace ubc {
     // basic math
     void uuint_t::Add(const uuint_t &n)
     {
-        throw std::runtime_error("Not Implemented");
+        details::UIntInternal remainder{0};
+
+        if (_chunks.Len() < n._chunks.Len())
+        {
+            // ...extent *this and fill it with zeros
+            for (size_t i = _chunks.Len(); i < n._chunks.Len(); i++)
+            {
+                _chunks.At(i) = 0;
+            }
+        }
+        for (size_t i = 0; i < std::max(_chunks.Len(), n._chunks.Len()); i++)
+        {
+            // mLen always bigger or equal to huge.mLen
+            details::UIntInternal nChunk{(i < n._chunks.Len()) ? n._chunks.At(i) : static_cast<details::UIntInternal>(0)};
+
+            // sum of remainder, huge value and *this
+            details::UIntInternal sum = _chunks.At(i) + nChunk + remainder;
+
+            _chunks.At(i) = sum % details::uintInternalBase;
+            remainder = sum / details::uintInternalBase;
+
+            // if no remainder left and huge.mLen is already walked thru then we calculated addition
+            // we can return here
+            if (!remainder && i >= n._chunks.Len())
+                return;
+        }
+
+        if (remainder)
+        {
+            _chunks.At(_chunks.Len()) = remainder;
+        }
     }
 
     void uuint_t::Multiply(const details::UIntInternal n)
